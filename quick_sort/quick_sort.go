1620609accf325101ad44a46c963185e4d832b17
Vlad S <v.sadkov@andersenlab.com>
2022-09-13 22:01:48 +0300

    quick search

---
+++ quick_sort/quick_sort.go
@@ -0,0 +1,23 @@
+package quick_sort
+
+import "math/rand"
+
+func QSort(list []int) {
+	if len(list) < 2 {
+		return
+	}
+	mid := rand.Int() % len(list)
+
+	left, right := 0, len(list)-1
+	list[right], list[mid] = list[mid], list[right]
+
+	for i, _ := range list {
+		if list[i] < list[right] {
+			list[left], list[i] = list[i], list[left]
+			left++
+		}
+	}
+	list[right], list[left] = list[left], list[right]
+	QSort(list[:left])
+	QSort(list[left+1:])
+}
