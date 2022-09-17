9ea6bc064543ea44e425e019c316635aafe89edb
Vlad S <v.sadkov@andersenlab.com>
2022-09-17 14:55:18 +0300

    fixed inserting sort

--- shell_sort/shell_sort.go
+++ shell_sort/shell_sort.go
@@ -8,14 +8,13 @@ func ShellSort(list []int) {
 		d = d / 2
 	}
 	for _, cp := range caps {
-		k := 0
-		for i := 0; i < len(list); i += cp {
-			k = i - cp
-			for k > -1 {
-				if list[k+cp] < list[k] {
-					list[k], list[k+cp] = list[k+cp], list[k]
+		for i := cp; i < len(list); i += cp {
+			j := i
+			for j > 0 {
+				if list[j-cp] > list[j] {
+					list[j-cp], list[j] = list[j], list[j-cp]
 				}
-				k -= cp
+				j = j - cp
 			}
 		}
 	}
